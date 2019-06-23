763089a48fffc384751238693cd902a3ee21ad57
Jose Arriaga <josearriag@hotmail.com>
2019-06-23 17:42:35 -0400

    Add jsp support. Add servlet to test AdminLTE.

---
+++ trms/src/main/java/com/revature/trms/servlets/AdminLteTestServlet.java
@@ -0,0 +1,22 @@
+package com.revature.trms.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminLteTestServlet
+ */
+public class AdminLteTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("adminlte-test.jsp").forward(request, response);
+	}
+
+}
