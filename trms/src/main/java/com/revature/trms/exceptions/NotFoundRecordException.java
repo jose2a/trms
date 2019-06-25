e9cacaae11604591f026b0775e818820a879202f
Jose Arriaga <josearriag@hotmail.com>
2019-06-24 22:24:47 -0400

    Add two more exceptions when a record is not found or when it's already in the db.

---
+++ trms/src/main/java/com/revature/trms/exceptions/NotFoundRecordException.java
@@ -0,0 +1,35 @@
+package com.revature.trms.exceptions;
+
+public class NotFoundRecordException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8259538061689607256L;
+
+	public NotFoundRecordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundRecordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundRecordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundRecordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundRecordException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ trms/src/main/java/com/revature/trms/exceptions/PreexistingRecordException.java
@@ -0,0 +1,35 @@
+package com.revature.trms.exceptions;
+
+public class PreexistingRecordException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PreexistingRecordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PreexistingRecordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PreexistingRecordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PreexistingRecordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PreexistingRecordException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
