77d93cb5917479d7f63da5d5b57150f95e06f918
Jose Arriaga <josearriag@hotmail.com>
2019-06-26 21:18:19 -0400

    Add getAttachmentDAO to DAOUtilities

--- trms/src/main/java/com/revature/trms/utilities/DAOUtilities.java
+++ trms/src/main/java/com/revature/trms/utilities/DAOUtilities.java
@@ -1,5 +1,7 @@
 package com.revature.trms.utilities;
 
+import com.revature.trms.daos.AttachmentDAO;
+import com.revature.trms.daos.AttachmentDAOImpl;
 import com.revature.trms.daos.EmployeeDAO;
 import com.revature.trms.daos.EmployeeDAOImpl;
 import com.revature.trms.daos.EmployeeTypeDAO;
@@ -7,10 +9,14 @@ import com.revature.trms.daos.EmployeeTypeDAOImpl;
 
 public class DAOUtilities {
 
+	public static AttachmentDAO getAttachmentDAO() {
+		return new AttachmentDAOImpl();
+	}
+
 	public static EmployeeDAO getEmployeeDAO() {
 		return new EmployeeDAOImpl();
 	}
-	
+
 	public static EmployeeTypeDAO getEmployeeTypeDAO() {
 		return new EmployeeTypeDAOImpl();
 	}
