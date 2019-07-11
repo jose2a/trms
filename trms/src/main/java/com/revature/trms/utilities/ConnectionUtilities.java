a5750fa59efb878d67b128db306b1c5ed9189ad6
Jose Arriaga <josearriag@hotmail.com>
2019-07-10 23:31:13 -0400

    Modified ConnectionUtilities to connect to DataSource. And updated .gitignore

--- trms/src/main/java/com/revature/trms/utilities/ConnectionUtilities.java
+++ trms/src/main/java/com/revature/trms/utilities/ConnectionUtilities.java
@@ -11,65 +11,19 @@ import javax.sql.DataSource;
 public class ConnectionUtilities {
 
 	private static Connection connection;
-//
-//	private static String url;
-//	private static String user;
-//	private static String password;
-//	private static final String PROPERTIES_FILE = "database.properties";
-//
-//	public static synchronized Connection getConnection() {
-//		try {
-//			if (connection == null || connection.isClosed()) {
-//				LogUtilities.trace("Connection is null or closed.");
-//				
-//				loadConectionsParameters();
-//				Class.forName("org.postgresql.Driver");
-//				connection = DriverManager.getConnection(url, user, password);
-//
-//				LogUtilities.trace("Connection open.");
-//			}
-//		} catch (ClassNotFoundException e) {
-//			LogUtilities.error("Could not register the driver.");
-//		} catch (SQLException e) {
-//			LogUtilities.error("Could not connect to the database.");
-//		}
-//
-//		LogUtilities.trace("Returning existing connection.");
-//
-//		return connection;
-//	}
-//
-//	private static void loadConectionsParameters() {
-//		LogUtilities.trace("Loading connection parameters from file.");
-//		Properties prop = new Properties();
-//
-//		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-//
-//		try (InputStream fis = loader.getResourceAsStream(PROPERTIES_FILE);) {
-//			prop.load(fis);
-//
-//			url = prop.getProperty("url");
-//			user = prop.getProperty("user");
-//			password = prop.getProperty("password");
-//		} catch (FileNotFoundException e) {
-//			LogUtilities.error("Connection file not loaded." + e.getMessage());
-//		} catch (IOException e) {
-//			LogUtilities.error("Could not load connection properties file. " + e.getMessage());
-//		}
-//	}
 
 	private static DataSource dataSource;
 
 	private static DataSource getDataSource() {
 		try {
 			Context initCtx = new InitialContext();
-			Context envCtx = (Context) initCtx.lookup("java:comp/env");
 
 			// Look up our data source
-			dataSource = (DataSource) envCtx.lookup("ds/trms");
+			dataSource = (DataSource) initCtx.lookup("java:comp/env/ds/trms");
+			
+			LogUtilities.trace("Getting datasource");
 		} catch (NamingException e) {
-			// Handle error that it's not configured in JNDI.
-			throw new IllegalStateException("jdbc/trms is missing in JNDI!", e);
+			LogUtilities.error(e.getMessage());
 		}
 
 		return dataSource;
@@ -78,10 +32,12 @@ public class ConnectionUtilities {
 	public static synchronized Connection getConnection() {
 		try {
 			connection = getDataSource().getConnection();
+			LogUtilities.trace("Got connection from Data Source");
 		} catch (SQLException e) {
 			LogUtilities.error("Could not connect to the database.");
 		}
 
+		LogUtilities.trace("Returning connection");
 		return connection;
 	}
 }
